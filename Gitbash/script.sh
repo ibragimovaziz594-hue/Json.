c8bdb46ad9a6cf8d705b5585b765be9e6215cd77
«Aziz <ibragimovaziz594@gmail.com>
2025-09-19 17:46:19 +0400

    modified:   script/script.sh

--- Gitbash/script.sh
+++ Gitbash/script.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 pwd
-mkdir {p1, p2, p3}
+mkdir p1 p2 p3
 cd p1
-touch {1.json,2.json,1.txt,2.txt}
+touch 1.json 2.json 1.txt 2.txt
 ls -la
 cat >> 1.txt
-cd../..
+cd../
 mv p1/{1.txt, 2.txt}p2
-cp p1/{1.txt, 2.txt}p2
-rm 1.json
+cp p1/{1.json, 2.json}p2
 history
-
+date
